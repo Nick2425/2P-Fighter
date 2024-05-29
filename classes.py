db1576d2ec8a5a7399a98be4620ca0c7d92eb195
Nick2425 <kuijpersnick2@gmail.com>
2024-05-28 23:02:34 -0400

    u2

--- classes.py
+++ classes.py
@@ -1,5 +1,34 @@
+import pygame
 # Player class is to create moveable player objects.
 class Player():
-  def __init__(self, x, y):
+  def __init__(self, x, y, h, w, c, t):
     self.x = x
     self.y = y
+    self.w = w
+    self.h = h
+    self.color = c
+    self.mt = t
+
+  def draw(self, win):
+    obj = (self.x, self.y, self.w, self.h)
+    c = self.color
+    pygame.draw.rect(win, c, obj) # Parameter 3 -> Rect which is a location, and a size. # Parameter 2 -> RGB # Parameter 1 -> Surface / Layer
+  def move(self, keys, v):
+    if self.mt == 1:
+      if keys[pygame.K_d]:
+          self.x += v
+      elif keys[pygame.K_a]:
+          self.x -= v
+      elif keys[pygame.K_w]:
+          self.y -= v
+      elif keys[pygame.K_s]:
+          self.y += v
+    elif self.mt == 2:
+      if keys[pygame.K_RIGHT]:
+          self.x += v
+      elif keys[pygame.K_LEFT]:
+          self.x -= v
+      elif keys[pygame.K_UP]:
+          self.y -= v
+      elif keys[pygame.K_DOWN]:
+          self.y += v
--- main.py
+++ main.py
@@ -6,13 +6,18 @@ pygame.init() #starts program
 win = pygame.display.set_mode((500, 500)) # sets window size.
 pygame.display.set_caption("Heyo") # sets window caption
 
+
 x = 50
 y = 50
+w = 50
+h = 50
+c = (255, 0, 0)
+#Creating player objects.
+player1 = classes.Player(x, y, w, h, c, 1)
+player1.draw(win)
 
-w = 40
-h = 60
-
-v = 5
+player2 = classes.Player(x+150, y, w, h, (0, 255, 0), 2)
+player2.draw(win)
 
 run = True
 while run:
@@ -21,17 +26,16 @@ while run:
         if event.type == pygame.QUIT: # event type is 'quit'
             run = False
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_d]:
-        x += v
-    elif keys[pygame.K_a]:
-        x -= v
-    elif keys[pygame.K_w]:
-        y -= v
-    elif keys[pygame.K_s]:
-        y += v
 
+    player1.move(keys, 10) # Diff move.
+    player2.move(keys, 10)
+
+
+    #!! Window update !!
     win.fill((0,0,0)) # Reset background -
-    pygame.draw.rect(win, (255, 0, 0), (x, y, w, h)) # Parameter 3 -> Rect which is a location, and a size. # Parameter 2 -> RGB # Parameter 1 -> Surface / Layer
+    player1.draw(win) # Printing players.
+    player2.draw(win)
+
     pygame.display.update()
 
 pygame.quit()
