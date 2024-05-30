4b439e3a02c0c98a33cb0e5657e3a01e6a0889e2
Nick2425 <kuijpersnick2@gmail.com>
2024-05-30 22:26:29 +0000

    U6

--- classes.py
+++ classes.py
@@ -1,27 +1,89 @@
 import pygame
 import os
 import constants
+import math
 
-cga_r = [pygame.transform.smoothscale_by(pygame.image.load(os.path.join('graphics/cg/cg-splice', 'row-2-column-' + str(i)) + '.png'), 1.5) for i in range(1,5)]
-cga_l = [pygame.transform.smoothscale_by(pygame.image.load(os.path.join('graphics/cg/cg-splice', 'row-4-column-' + str(i)) + '.png'), 1.5) for i in range(1,5)]
+cga_r = [
+    pygame.transform.smoothscale_by(
+        pygame.image.load(
+            os.path.join('graphics/cg/cg-splice', 'row-2-column-' + str(i)) +
+            '.png'), 1.5) for i in range(1, 5)
+]
+cga_l = [
+    pygame.transform.smoothscale_by(
+        pygame.image.load(
+            os.path.join('graphics/cg/cg-splice', 'row-4-column-' + str(i)) +
+            '.png'), 1.5) for i in range(1, 5)
+]
 # Still Images
-cgs_r = pygame.transform.smoothscale_by(pygame.image.load(os.path.join('graphics/cg/cg-splice', 'row-2-column-1.png')), 1.5)
-cgs_l = pygame.transform.smoothscale_by(pygame.image.load(os.path.join('graphics/cg/cg-splice', 'row-4-column-1.png')), 1.5)
+cgs_r = pygame.transform.smoothscale_by(
+    pygame.image.load(
+        os.path.join('graphics/cg/cg-splice', 'row-2-column-1.png')), 1.5)
+cgs_l = pygame.transform.smoothscale_by(
+    pygame.image.load(
+        os.path.join('graphics/cg/cg-splice', 'row-4-column-1.png')), 1.5)
 # Green Character Left / Right Movement Images for animation
-cba_r = [pygame.transform.smoothscale_by(pygame.image.load(os.path.join('graphics/cb/cb-splice', 'row-2-column-' + str(i)) + '.png'), 1.5) for i in range(1,5)]
-cba_l = [pygame.transform.smoothscale_by(pygame.image.load(os.path.join('graphics/cb/cb-splice', 'row-4-column-' + str(i)) + '.png'), 1.5) for i in range(1,5)]
+cba_r = [
+    pygame.transform.smoothscale_by(
+        pygame.image.load(
+            os.path.join('graphics/cb/cb-splice', 'row-2-column-' + str(i)) +
+            '.png'), 1.5) for i in range(1, 5)
+]
+cba_l = [
+    pygame.transform.smoothscale_by(
+        pygame.image.load(
+            os.path.join('graphics/cb/cb-splice', 'row-4-column-' + str(i)) +
+            '.png'), 1.5) for i in range(1, 5)
+]
 # Still Images
-cbs_r = pygame.transform.smoothscale_by(pygame.image.load(os.path.join('graphics/cb/cb-splice', 'row-2-column-1.png')), 1.5)
-cbs_l = pygame.transform.smoothscale_by(pygame.image.load(os.path.join('graphics/cb/cb-splice', 'row-4-column-1.png')), 1.5)
+cbs_r = pygame.transform.smoothscale_by(
+    pygame.image.load(
+        os.path.join('graphics/cb/cb-splice', 'row-2-column-1.png')), 1.5)
+cbs_l = pygame.transform.smoothscale_by(
+    pygame.image.load(
+        os.path.join('graphics/cb/cb-splice', 'row-4-column-1.png')), 1.5)
 # Player class is to create moveable player objects.
 # Attack animations
-cgat_l = [pygame.transform.smoothscale_by(pygame.image.load(os.path.join('graphics/cg/cg-attack-splice', 'row-4-column-' + str(i) + '.png')), 1.5) for i in range(1,5)]
-cgat_r = [pygame.transform.smoothscale_by(pygame.image.load(os.path.join('graphics/cg/cg-attack-splice', 'row-3-column-' + str(i) + '.png')), 1.5) for i in range(1,5)]
+cgat_l = [
+    pygame.transform.smoothscale_by(
+        pygame.image.load(
+            os.path.join('graphics/cg/cg-attack-splice',
+                         'row-4-column-' + str(i) + '.png')), 1.5)
+    for i in range(1, 5)
+]
+cgat_r = [
+    pygame.transform.smoothscale_by(
+        pygame.image.load(
+            os.path.join('graphics/cg/cg-attack-splice',
+                         'row-3-column-' + str(i) + '.png')), 1.5)
+    for i in range(1, 5)
+]
 
-cbat_l = [pygame.transform.smoothscale_by(pygame.image.load(os.path.join('graphics/cb/cb-attack-splice', 'row-4-column-' + str(i) + '.png')), 1.5) for i in range(1,5)]
-cbat_r = [pygame.transform.smoothscale_by(pygame.image.load(os.path.join('graphics/cb/cb-attack-splice', 'row-3-column-' + str(i) + '.png')), 1.5) for i in range(1,5)]
+cbat_l = [
+    pygame.transform.smoothscale_by(
+        pygame.image.load(
+            os.path.join('graphics/cb/cb-attack-splice',
+                         'row-4-column-' + str(i) + '.png')), 1.5)
+    for i in range(1, 5)
+]
+cbat_r = [
+    pygame.transform.smoothscale_by(
+        pygame.image.load(
+            os.path.join('graphics/cb/cb-attack-splice',
+                         'row-3-column-' + str(i) + '.png')), 1.5)
+    for i in range(1, 5)
+]
+
+animation_const = 0
+
+
+def dist(obj1, obj2):
+  dist = math.sqrt((obj1.x - obj2.x)**2 + (obj1.y - obj2.y)**2)
+  return dist
+
 
 class Player():
+
   def __init__(self, x, y, h, w, c, t):
     self.x = x
     self.y = y
@@ -29,16 +91,18 @@ class Player():
     self.h = h
     self.color = c
     self.dir = 0
-    self.mt = t #movement type: differing keyboard inputs.
-  def anim(self, win, type, direction):
-    ticks = pygame.time.get_ticks()
-    x = ticks % 4 
+    self.mt = t  #movement type: differing keyboard inputs.
+    self.ac = 0
+    self.attack = False
+
+  def anim(self, win, type, direction, obj1, obj2):
+    x = animation_const % 4
     if type == 1:
       # Direction is facing left or right
       if direction == 'r':
         win.blit(cga_r[x], (self.x, self.y))
       elif direction == 'l':
-        win.blit(cga_l[x], (self.x, self.y)) 
+        win.blit(cga_l[x], (self.x, self.y))
       elif direction == 'rs':
         win.blit(cgs_r, (self.x, self.y))
       elif direction == 'ls':
@@ -47,66 +111,90 @@ class Player():
     elif type == 2:
       if direction == 'l':
         win.blit(cba_l[x], (self.x, self.y))
-      elif direction == 'r':    
+      elif direction == 'r':
         win.blit(cba_r[x], (self.x, self.y))
       elif direction == 'rs':
         win.blit(cbs_r, (self.x, self.y))
       elif direction == 'ls':
         win.blit(cbs_l, (self.x, self.y))
 
-    # Attack
+    # Attack animation.
     if type == 3:
-      if direction == 'r' or 'rs':
-        pass
-      elif direction == 'l' or 'ls':
-        pass
+      if self.dir == "rs":
+        win.blit(cgat_r[self.ac], (self.x, self.y))
+      elif self.dir == 'r':
+        win.blit(cgat_r[self.ac], (self.x, self.y))
+      elif self.dir == "ls":
+        win.blit(cgat_l[self.ac], (self.x, self.y))
+      elif self.dir == 'l':
+        win.blit(cgat_l[self.ac], (self.x, self.y))
+      if dist(obj1, obj2) < 50:
+        print("p1 wins")
     elif type == 4:
-      if direction == 'r' or 'rs':
-        pass
-      elif direction == 'l' or 'ls':
-        pass
+      if self.dir == "rs":
+        win.blit(cbat_r[self.ac], (self.x, self.y))
+      elif self.dir == 'r':
+        win.blit(cbat_r[self.ac], (self.x, self.y))
+      elif self.dir == "ls":
+        win.blit(cbat_l[self.ac], (self.x, self.y))
+      elif self.dir == 'l':
+        win.blit(cbat_l[self.ac], (self.x, self.y))
+      if dist(obj1, obj2) < 50:
+        print("p2 wins")
 
-
-
-
-  def draw(self, win):
+  def draw(self, win, obj1, obj2):
     #obj = (self.x, self.y, self.w, self.h)
     #c = self.color
     #pygame.draw.rect(win, c, obj) # Parameter 3 -> Rect which is a location, and a size. # Parameter 2 -> RGB # Parameter 1 -> Surface / Layer
     #Drawing Character
-    self.anim(win, self.mt, self.dir)
+    if self.attack == False:
+      self.anim(win, self.mt, self.dir, obj1, obj2)
+    elif self.attack:
+      self.anim(win, self.mt + 2, self.dir, obj1, obj2)
 
-
-  def move(self, keys, v): # Defines different game object movements.
-    if self.mt == 1:
-      if keys[pygame.K_d] and self.x + v <= constants.RIGHT_LIMIT - self.w*0.75:
+  def move(self, keys, v):  # Defines different game object movements.
+    if self.attack != True:
+      if self.mt == 1:
+        if keys[pygame.K_q]:
+          self.attack = True
+          self.ac = 0
+        elif keys[pygame.
+                  K_d] and self.x + v <= constants.RIGHT_LIMIT - self.w * 0.75:
           self.x += v
           self.dir = 'r'
-      elif keys[pygame.K_a] and self.x - v >= constants.LEFT_LIMIT:
+        elif keys[pygame.K_a] and self.x - v >= constants.LEFT_LIMIT:
           self.x -= v
           self.dir = 'l'
-      elif keys[pygame.K_w] and self.y - v >= constants.UPPER_LIMIT:
+        elif keys[pygame.K_w] and self.y - v >= constants.UPPER_LIMIT:
           self.y -= v
-      elif keys[pygame.K_s] and self.y + v <= constants.LOWER_LIMIT - 0.5*self.h:
+        elif keys[
+            pygame.K_s] and self.y + v <= constants.LOWER_LIMIT - 0.5 * self.h:
           self.y += v
-      else:
-        if self.dir == 'l':
-          self.dir = "ls"
-        elif self.dir == 'r':
-          self.dir = "rs"
-    elif self.mt == 2:
-      if keys[pygame.K_RIGHT] and self.x + v <= constants.RIGHT_LIMIT - self.w*0.75:
+        else:
+          if self.dir == 'l':
+            self.dir = "ls"
+          elif self.dir == 'r':
+            self.dir = "rs"
+      elif self.mt == 2:
+        if keys[pygame.K_RSHIFT]:
+          self.attack = True
+          self.ac = 0
+        elif keys[
+            pygame.
+            K_RIGHT] and self.x + v <= constants.RIGHT_LIMIT - self.w * 0.75:
           self.x += v
           self.dir = 'r'
-      elif keys[pygame.K_LEFT] and self.x - v >= constants.LEFT_LIMIT:
+        elif keys[pygame.K_LEFT] and self.x - v >= constants.LEFT_LIMIT:
           self.x -= v
           self.dir = 'l'
-      elif keys[pygame.K_UP] and self.y - v >= constants.UPPER_LIMIT:
+        elif keys[pygame.K_UP] and self.y - v >= constants.UPPER_LIMIT:
           self.y -= v
-      elif keys[pygame.K_DOWN] and self.y + v <= constants.LOWER_LIMIT - 0.5*self.h:
+        elif keys[
+            pygame.
+            K_DOWN] and self.y + v <= constants.LOWER_LIMIT - 0.5 * self.h:
           self.y += v
-      else:
-        if self.dir == 'l':
-          self.dir = "ls"
-        elif self.dir == 'r':
-          self.dir = "rs"
+        else:
+          if self.dir == 'l':
+            self.dir = "ls"
+          elif self.dir == 'r':
+            self.dir = "rs"
--- main.py
+++ main.py
@@ -2,17 +2,15 @@ import pygame
 import classes, functions
 import os
 
-pygame.init() #starts program
+pygame.init()  #starts program
 
-win = pygame.display.set_mode((480, 270)) # sets window size.
-pygame.display.set_caption("Heyo") # sets window caption
+win = pygame.display.set_mode((480, 270))  # sets window size.
+pygame.display.set_caption("Heyo")  # sets window caption
 
 # Images
 bg = pygame.image.load(os.path.join('graphics', 'bg.png'))
 
-
 clock = pygame.time.Clock()
-
 
 x = 50
 y = 50
@@ -20,29 +18,51 @@ w = 50
 h = 50
 c = (255, 0, 0)
 #Creating player objects.
-player1 = classes.Player(x+80, y+150, w, h, c, 1)
-player1.draw(win)
+player1 = classes.Player(x + 80, y + 150, w, h, c, 1)
+player2 = classes.Player(x + 300, y + 150, w, h, (0, 255, 0), 2)
+
+player1.draw(win, player1, player2)
 player1.dir = 'rs'
-player2 = classes.Player(x+300, y+150, w, h, (0, 255, 0), 2)
-player2.draw(win)
+player2.draw(win, player1, player2)
 player2.dir = 'ls'
 
+# Custom Event to make a coroutine.
+C_E = pygame.event.custom_type()
+
 run = True
+pygame.time.set_timer(C_E, 250)
+
 while run:
-    clock.tick() # 40 ticks per second
-    pygame.time.delay(25) # delays code by 0.025s
-    for event in pygame.event.get(): # gets list of events
-        if event.type == pygame.QUIT: # event type is 'quit'
-            run = False
+    clock.tick()  # 40 ticks per second
+    pygame.time.delay(25)  # delays code by 0.025s
+
     keys = pygame.key.get_pressed()
-    player1.move(keys, 3) # Diff move.
+    player1.move(keys, 3)  # Diff move.
     player2.move(keys, 3)
 
     #!! Window update !!
-    win.blit(bg, (0,0)) # Reset background -
-    player1.draw(win) # Printing players.
-    player2.draw(win)
+    win.blit(bg, (0, 0))  # Reset background -
+    player1.draw(win, player1, player2)  # Printing players.
+    player2.draw(win, player1, player2)
 
     pygame.display.update()
+    for event in pygame.event.get():  # gets list of events
+        if event.type == pygame.QUIT:  # event type is 'quit'
+            run = False
+        # Called every 0.5s / 20 ticks.
+        if event.type == C_E:
+            classes.animation_const += 1
 
+            if player1.ac == 3:
+                player1.ac = 0
+                player1.attack = False
+            if player2.ac == 3:
+                player2.ac = 0
+                player2.attack = False
+
+            if player1.attack:
+                player1.ac += 1
+
+            if player2.attack:
+                player2.ac += 1
 pygame.quit()
